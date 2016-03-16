bc5bbd31110ad885bc61e55b2c7a2abc89120116
Patrick Allain <patralla@gmail.com>
2016-03-16 18:26:21 +0100

    Add spock in project

---
+++ src/main/java/fr/soat/cm/CoffeeMachine.java
@@ -0,0 +1,10 @@
+package fr.soat.cm;
+
+/**
+ * Coffee machine.
+ *
+ * @author patouche - 4/15/16.
+ */
+public class CoffeeMachine {
+
+}
---
+++ src/test/groovy/fr/soat/cm/CoffeeMachineTest.groovy
@@ -0,0 +1,12 @@
+package fr.soat.cm
+
+import spock.lang.Specification
+
+class CoffeeMachineTest extends Specification {
+
+    def test() {
+        expect:
+            0 == 0
+    }
+
+}
